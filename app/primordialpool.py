7462d31fc2619ae97d9e6c778b2c5e55017b1172
Joshua C. Burt <joshburt@shapeandshare.com>
2017-09-11 17:55:06 -0600

    migrated from https://github.com/shapeandshare/dicebox

---
+++ app/primordialpool.py
@@ -0,0 +1,123 @@
+# Derived from https://github.com/harvitronix/neural-network-genetic-algorithm
+# Derived source copyright: Matt Harvey, 2017, Derived source license: The MIT License
+# See docs/Matt Harvey.LICENSE
+
+"""Entry point to evolving the neural network. Start here."""
+import logging
+from lib.optimizer import Optimizer
+from tqdm import tqdm
+import lib.dicebox_config as config
+
+
+# Setup logging.
+logging.basicConfig(
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%m/%d/%Y %I:%M:%S %p',
+    level=logging.INFO,
+    filemode='w',
+    filename="%s/primordialpool.log" % config.LOGS_DIR
+)
+
+
+def train_networks(networks, dataset):
+    """Train each network.
+
+    Args:
+        networks (list): Current population of networks
+        dataset (str): Dataset to use for training/evaluating
+    """
+    pbar = tqdm(total=len(networks))
+    for network in networks:
+        network.train(dataset)
+        pbar.update(1)
+    pbar.close()
+
+
+def get_average_accuracy(networks):
+    """Get the average accuracy for a group of networks.
+
+    Args:
+        networks (list): List of networks
+
+    Returns:
+        float: The average accuracy of a population of networks.
+
+    """
+    total_accuracy = 0
+    for network in networks:
+        total_accuracy += network.accuracy
+
+    return total_accuracy / len(networks)
+
+
+def generate(generations, population, nn_param_choices, dataset):
+    """Generate a network with the genetic algorithm.
+
+    Args:
+        generations (int): Number of times to evole the population
+        population (int): Number of networks in each generation
+        nn_param_choices (dict): Parameter choices for networks
+        dataset (str): Dataset to use for training/evaluating
+
+    """
+    optimizer = Optimizer(nn_param_choices)
+    networks = optimizer.create_population(population)
+
+    # Evolve the generation.
+    for i in range(generations):
+        logging.info("***Doing generation %d of %d***" %
+                     (i + 1, generations))
+        logging.info('-'*80)
+        logging.info('Individuals in current generation')
+        print_networks(networks)
+        logging.info('-' * 80)
+
+        # Train and get accuracy for networks.
+        train_networks(networks, dataset)
+
+        # Get the average accuracy for this generation.
+        average_accuracy = get_average_accuracy(networks)
+
+        # Print out the average accuracy each generation.
+        logging.info("Generation average: %.2f%%" % (average_accuracy * 100))
+        logging.info('-'*80)
+
+        logging.info('Top 5 individuals in current generation')
+
+        # Sort our final population.
+        current_networks = sorted(networks, key=lambda x: x.accuracy, reverse=True)
+
+        # Print out the top 5 networks.
+        print_networks(current_networks[:5])
+
+        # Evolve, except on the last iteration.
+        if i != generations - 1:
+            # Do the evolution.
+            networks = optimizer.evolve(networks)
+
+    # Sort our final population.
+    networks = sorted(networks, key=lambda x: x.accuracy, reverse=True)
+
+    # Print out the top 5 networks.
+    print_networks(networks[:5])
+
+
+def print_networks(networks):
+    """Print a list of networks.
+
+    Args:
+        networks (list): The population of networks
+
+    """
+    logging.info('-'*80)
+    for network in networks:
+        network.print_network()
+
+
+def main():
+    logging.info("***Evolving %d generations with population %d***" % (config.GENERATIONS, config.POPULATION))
+    generate(config.GENERATIONS, config.POPULATION, config.NN_PARAM_CHOICES, config.DATASET)
+
+
+if __name__ == '__main__':
+    main()
